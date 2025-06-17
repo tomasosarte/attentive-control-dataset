8e25dc1db99433bd866a8cb61680ad3e654ad69d
tomasosarte <tomasosarte@gmail.com>
2025-06-17 11:53:58 +0200

    Added placeholder invariances

---
+++ src/invariances/invariance.py
@@ -0,0 +1,4 @@
+class Invariance:
+
+    def __init__(self):
+        pass
---
+++ src/invariances/rotation.py
@@ -0,0 +1,6 @@
+from src.invariances.invariance import Invariance
+
+class Rotation(Invariance):
+
+    def __init__(self):
+        super().__init__()
